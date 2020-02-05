d7bfea43cf2bee112fe6207dee6f7902f590f15e
Oleg Chulkov <pilot114@bk.ru>
2020-02-06 02:26:39 +0700

    fuck fuck fuck

--- server/external/habr.js
+++ server/external/habr.js
@@ -6,9 +6,7 @@ let domain = "https://habr.com";
 let url = domain + "/ru/users/pilot114/favorites/page1/";
 // https://habr.com/ru/users/pilot114/favorites/tag/javascript/
 
-/*
-    Рекурсивно парсит DOM, пока только текст
-*/
+
 // function buildFromDom($, format) {
 //     let page = {};
 //     let root = $('.content-list');
@@ -37,66 +35,62 @@ let url = domain + "/ru/users/pilot114/favorites/page1/";
 let htmlFormat = {
     selector: '.content-list',
     posts: {
-        selector: '.post',
-        // title: {
-        //     selector: '.post__title'
-        // },
-        // links: {
-        //     selector: '.hub-links',
-        // }
+        selector: ['.post'],
+        title: {
+            selector: '.post__title'
+        },
+        links: {
+            selector: ['.hub-link'],
+        }
     }
 };
-// TODO
-function buildFromDom($, format, data, node) {
-    data = data || {};
-    console.log('query node: ', format.selector);
-    nodes = $(format.selector, node);
+/*
+    Рекурсивно парсит DOM, пока только текст
+*/
+function buildFromDom($, format, node) {
+    node = node || null;
 
-    $(node).each(function(i, item){
-        console.log(item.name);
-    });
-
+    let selector = format.selector;
     delete format.selector;
 
-    let keys = Object.keys(format);
+    nodes = $(selector, node);
 
-    //  leaf case
+    // если больше нет ключей, nodes - это данные, выбираем и выходим (может быть массивом)
+    let keys = Object.keys(format);
+    let items = [];
+
     if (keys.length === 0) {
-        let items = [];
-        $(node).each(function(i, item){
-            console.log('query leaf: ', format.selector);
-            items.push($(format.selector, item).text().trim());
+        selector = Array.isArray(selector) ? selector.shift() : selector;
+        console.log('query leaf: ', selector);
+
+        $(nodes).each(function(i, item){
+            items.push($(item).text().trim());
         });
         return items.length < 2 ? items.shift() : items;
+    // есть вложенные поля - применяем рекурсию
+    } else {
+        let data = {};
+        for (let i in keys) {
+            let field = keys[i];
+
+            if (Array.isArray(format[field].selector)) {
+                selector = format[field].selector.shift();
+                console.log('query node: ', selector);
+
+                let arr = [];
+                $($(selector, node)).each(function(i, item){
+                    arr.push(buildFromDom($, format[field], item));
+                });
+                data[field] = arr;
+            } else {
+                console.log('query node: ', selector);
+                // если в функцию передаётся массив нод, она тоже вернёт массив
+                data[field] = buildFromDom($, format[field], nodes);
+            }
+        }
+
+        return data;
     }
-
-    for (let i in keys) {
-        data[keys[i]] = buildFromDom($, format[keys[i]], {}, node);
-    }
-
-    return data;
-
-    // let page = {};
-    // let root = $('.content-list');
-    //
-    // page.posts = [];
-    //
-    // let posts = $('.post', root);
-    // $(posts).each(function(i, post){
-    //     let postData = {};
-    //
-    //     postData.title = $('.post__title', post).text().trim();
-    //     postData.links = [];
-    //
-    //     let links = $('.hub-link', post);
-    //     $(links).each(function(i, link){
-    //         postData.links.push($(link).text().trim());
-    //     });
-    //
-    //     page.posts.push(postData);
-    // });
-    //
-    // return page;
 }
 
 function getFavorites() {
